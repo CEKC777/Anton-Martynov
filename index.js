f49fb95091afba0d40e02a2a2bbe6e14c513b8ec
Timur Ramazanov <timurram007@gmail.com>
2023-11-21 19:04:23 +0100

    removed fonts animation

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const links = document.querySelectorAll('a');
-    const fontList = [
-        'font1', 'font2', 'font3', 'font4'
-    ];
+// document.addEventListener('DOMContentLoaded', function () {
+//     const links = document.querySelectorAll('a');
+//     const fontList = [
+//         'font1', 'font2', 'font3', 'font4'
+//     ];
 
-    function applyRandomFont(link) {
-        const randomFontClass = fontList[Math.floor(Math.random() * fontList.length)];
-        link.classList = randomFontClass;
-    }
+//     function applyRandomFont(link) {
+//         const randomFontClass = fontList[Math.floor(Math.random() * fontList.length)];
+//         link.classList = randomFontClass;
+//     }
 
-    function changeFontsRepeatedly(link) {
-        let counter = 0;
-        const maxChanges = Math.floor(Math.random() * (10 - 5 + 1) + 5);
-        const intervalId = setInterval(() => {
-            applyRandomFont(link);
-            counter++;
+//     function changeFontsRepeatedly(link) {
+//         let counter = 0;
+//         const maxChanges = Math.floor(Math.random() * (10 - 5 + 1) + 5);
+//         const intervalId = setInterval(() => {
+//             applyRandomFont(link);
+//             counter++;
 
-            if (counter === maxChanges) {
-                clearInterval(intervalId);
-            }
-        }, 100); // Change every 100 milliseconds
-        return intervalId
-    }
+//             if (counter === maxChanges) {
+//                 clearInterval(intervalId);
+//             }
+//         }, 100); // Change every 100 milliseconds
+//         return intervalId
+//     }
 
-    links.forEach(link => {
-        let intervalId
-        if (link.id !== 'title') {
-            link.addEventListener('mouseenter', () => intervalId = changeFontsRepeatedly(link));
-            link.addEventListener('mouseleave', () => {
-                clearInterval(intervalId)
-                link.classList = []
-            });
-        }
-    });
-});
+//     links.forEach(link => {
+//         let intervalId
+//         if (link.id !== 'title') {
+//             link.addEventListener('mouseenter', () => intervalId = changeFontsRepeatedly(link));
+//             link.addEventListener('mouseleave', () => {
+//                 clearInterval(intervalId)
+//                 link.classList = []
+//             });
+//         }
+//     });
+// });
